7f4de758c2c86dfb7ef07a487fe2ee74d21e0b99
Pankaj <pankajpalai405@gmail.com>
2021-02-25 13:00:20 +0530

    oddsum and evensum

---
+++ 01NumberBasedProgramming/src/com/pk/np/Test06_OddSumEvenSum.java
@@ -0,0 +1,50 @@
+package com.pk.np;
+
+
+/*
+ *  PERFECT NUMBER: Perfect number, a positive integer that is equal to the sum of its proper divisors. excluding the number itself
+ *   The smallest perfect number is 6, which is the sum of 1, 2, and 3. Other perfect numbers are 28, 496, and 8,128.
+ **/
+public class Test06_OddSumEvenSum {
+
+	public static void main(String[] args) {
+
+		System.out.println(oddSum(3971742));
+
+		System.out.println(evenSum(642579));
+	}
+
+	private static int evenSum(int num) {
+		int rem=0;
+		int sum=0;
+		while(num>0) {
+			rem=num%10;
+			if(rem%2==0) {
+				sum=sum+rem;
+			}
+			num=num/10;
+		}
+			
+		
+		return sum;
+	}
+
+	private static int oddSum(int num) {
+		int rem=0;
+		int sum=0;
+		while(num>0) {
+			rem=num%10;
+			if(rem%2!=0) {
+				sum=sum+rem;
+			}
+			num=num/10;
+			
+		}
+		
+		return sum;
+	}
+
+	
+	}
+
+
