0a567781b2a3e95e5364f5f7f3827bca4999a93e
Pankaj <pankajpalai405@gmail.com>
2021-02-26 05:42:42 +0530

    Palindrome numbers

--- 01NumberBasedProgramming/src/com/pk/np/Test06_OddSumEvenSum.java
+++ 01NumberBasedProgramming/src/com/pk/np/Test06_OddSumEvenSum.java
@@ -1,6 +1,7 @@
 package com.pk.np;
 
 
+
 /*
  *  PERFECT NUMBER: Perfect number, a positive integer that is equal to the sum of its proper divisors. excluding the number itself
  *   The smallest perfect number is 6, which is the sum of 1, 2, and 3. Other perfect numbers are 28, 496, and 8,128.
---
+++ 01NumberBasedProgramming/src/com/pk/np/Test07_PalindromeNumber.java
@@ -0,0 +1,59 @@
+package com.pk.np;
+
+/*
+ *PALINDROME NUMBER:: A Palindrome number is a number by reversing the number we will get the same number
+ *eg: 525,7887,956659
+ *
+ **/
+public class Test07_PalindromeNumber {
+
+	public static void main(String[] args) {
+		System.out.println(checkPalindrome(75457));
+
+		printPalindrome(1,1000);
+
+	}
+	
+	//printing palindrome numbers in a given range
+
+	private static void printPalindrome(int startRange, int endRange) {
+		int num=0;
+		
+		for(int i=startRange;i<endRange;i++) {
+			int rem=0;
+			int revNum=0;
+			num=i;
+			while(num>0) {
+				rem=num%10;
+				revNum=revNum*10+rem;
+				num=num/10;
+			}
+			if(i==revNum) {
+				System.out.println(i);
+			}
+			
+		}
+		
+		
+		
+	}
+//checking whether a num is palindrome or not
+	private static boolean checkPalindrome(int num) {//434
+		int rem=0;
+		int revNum=0;
+		int temp=num;
+		while(num>0) {//434>0,43>0
+			rem=num%10;	//4,3,
+			revNum=revNum*10+rem;//4,43,
+			num=num/10;	//43,
+		}
+		
+		if(temp==revNum) {
+		return true;
+		}
+		return false;
+	}
+
+	}
+
+
